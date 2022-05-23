cb3ceb5e59a0cae3d3252933280aebb4d35ea876
Nguyen van Nguyen <nvnguyen2504@gmail.com>
2022-05-23 07:16:53 +0700

    There’s a mysql server for development which you can access using the following info IP: 178.128.109.9 Username: test01 Password: PlsDoNotShareThePass123@ Database: entrance_test

--- database/connectKnex.js
+++ database/connectKnex.js
@@ -1,18 +1,30 @@
 
-'use strict';
-require('dotenv').config({path:'./../.env'});
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-        host: '127.0.0.1',
-        port: 3306,
-        user: 'root',
-        password: 'Mothaiba@$%^',
-        database: 'entrance_test'
-    },
-    pool: { min: 0, max: 7 }
+"use strict";
+require("dotenv").config({ path: "./../.env" });
+// const knex = require("knex")({
+// 	client: "mysql",
+// 	connection: {
+// 		host: "127.0.0.1",
+// 		port: 3306,
+// 		user: "root",
+// 		password: "Mothaiba@$%^",
+// 		database: "entrance_test"
+// 	},
+// 	pool: { min: 0, max: 7 }
+// });
+const knex = require("knex")({
+	client: "mysql",
+	connection: {
+		host: "178.128.109.9",
+		port: 3306,
+		user: "root",
+		password: "PlsDoNotShareThePass123@",
+		database: "entrance_test"
+	},
+	pool: { min: 0, max: 7 }
 });
 
-console.log(process.env.HOST_TEST)
+// eslint-disable-next-line no-undef
+console.log(process.env.HOST_TEST);
 
-module.exports  = { knex }
+module.exports = { knex };
